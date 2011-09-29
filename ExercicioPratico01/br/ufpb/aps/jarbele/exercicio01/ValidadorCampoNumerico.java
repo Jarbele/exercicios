e360cab7d9078df2143f110c8e893a4b89825652
JARBELE <jarbele@dce.ufpb.br>
2011-09-29 19:15:48 -0300

    O exercício ainda está sendo concluído.

--- ExercicioPratico01/br/ufpb/aps/jarbele/exercicio01/ValidadorCampoNumerico.java
+++ ExercicioPratico01/br/ufpb/aps/jarbele/exercicio01/ValidadorCampoNumerico.java
@@ -15,23 +15,3 @@ public class ValidadorCampoNumerico implements ValidadorCampo {
 	}
 }
 
-/*
- * int idade = Integer.parseInt(texto);
-
-        //Definir a seqüência de caracteres padrão de numeros
-        Pattern p = Pattern.compile("[0-9]+");
-
-        //Combina a string dada com o padrão
-        Matcher m = p.matcher(texto);
-
-        //Verificar se a comparação é verdadeira
-        boolean matchFound = m.matches();
-        if (matchFound) {
-            System.out.println("OK");
-        } else {
-            if ((idade < 0) || (idade >= 150)){
-                    throw new IdadeInvalidaException("Idade inválida!");
-            }
-
-
- */
